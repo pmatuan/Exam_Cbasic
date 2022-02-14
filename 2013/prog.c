35b7bd5b333d2ddccd60a8e41fe2dc7d6a2c6477
pmatuan <tuan.pma205227@sis.hust.edu.vn>
2022-02-14 21:28:52 +0700

    Fix tính năng 4 2013

--- 2013/prog.c
+++ 2013/prog.c
@@ -1,10 +1,8 @@
 #include "all.c"
 #include "io.h"
 typedef char string[11];
-int a,b;
-string a1, b1;
-char *name, *name_1;
-int choice = -1, count = 0, X;
+int a, b, choice = -1, threshold = 1;
+string team1, team2, team;
 gtype *tmp;
 void menu(){
     printf("1. Tao cay\n");
@@ -15,77 +13,65 @@ void menu(){
     printf("6. Thoat\n");
 }
 int main(int argc, char *argv[]){
-    hmap_t map = hmap_create(gtype_hash_s, gtype_cmp_s, NULL, NULL);
-    bn_tree_t tree = bns_create_tree_g(NULL, gtype_cmp_s);
     FILE *f = fopen(argv[1], "r");
-    while(!feof(f)){
-        fscanf(f, "%s   %s  %d  %d", a1, b1, &a, &b);
-        hmap_ires res1 = hmap_insert(map, gtype_s(strdup(a1)), gtype_l(0));
-        hmap_ires res2 = hmap_insert(map, gtype_s(strdup(b1)), gtype_l(0));
-        if(a > b){
-            res1.value->l += 3;
-        }
-        else if(a < b){
-            res2.value->l += 3;
-        }
-        else if(a == b){
-            res1.value->l += 1;
-            res2.value->l += 1;
-        }
-    }
-    while(choice != 6){
+    rbm_t tree = rbm_create(gtype_cmp_s, NULL, NULL);
+    while(choice!=6){
         menu();
-        printf("Lua chon cua ban: ");
+        printf("Nhap vao lua chon cua ban: ");
         scanf("%d",&choice);
         if(choice == 1){
-            hmap_traverse(key,value,map){
-                count += 1;
-                bns_insert_g(tree, gtype_s(key->s));
-                printf("Nut: %s, nut thu %d, dia chi cua nut: %p\n", key->s, count, key->s);
+            while(!feof(f)){
+                fscanf(f,"%s\t%s\t%d\t%d", team1, team2, &a, &b);
+                rbm_ires res1 = rbm_insert(tree,gtype_s(strdup(team1)), gtype_l(0));
+                rbm_ires res2 = rbm_insert(tree,gtype_s(strdup(team2)), gtype_l(0));
+                if(a > b) res1.value->l+=3;
+                else if(a < b) res2.value->l+=3;
+                else {
+                    res1.value->l+=1;
+                    res2.value->l+=1;
+                }
             }
         }
         else if(choice == 2){
-            bn_traverse_lnr(cur,tree){
-                name = bns_node_g_key(cur).s;
-                tmp = hmap_value(map, gtype_s(strdup(name)));
-                printf("%s %ld\n",name, tmp->l);
+            rbm_traverse(k, v, tree){
+                printf("%s %ld\n",k->s,v->l);
             }
         }
         else if(choice == 3){
+            printf("Nhap ma doi bong: ");
+            scanf("%s",team);
             clear_stdin();
-            printf("Nhap vao ma doi can tim: ");
-            cgetline(&name_1, 0, stdin);
-            remove_tail_lf(name_1);
-            tmp = hmap_value(map, gtype_s(strdup(name_1)));
+            tmp = rbm_value(tree, gtype_s(team));
             if(tmp == NULL){
-                printf("Khong co doi bong nay!\n");
+                printf("Khong co doi bong nay\n");
             }
-            else{
-                printf("Diem doi nay la: %ld\n",tmp->l);
-            }
+            else printf("%s %ld\n",team, tmp->l);
         }
         else if(choice == 4){
-            printf("Nhap X: ");
-            scanf("%d", &X);
-            bn_traverse_lnr(cur, tree){
-                name = bns_node_g_key(cur).s;
-                tmp = hmap_value(map, gtype_s(strdup(name)));
-                if(X > tmp->l){
-                    printf("%s\n", bns_node_g_key(cur).s);
-                    bns_delete_g(tree, cur);
+            gvec_t vec = gvec_create(10, NULL);
+            printf("Nhap vao nguong: ");
+            scanf("%d",&threshold);
+            rbm_traverse(k, v, tree){
+                if(v->l < threshold){
+                    gvec_append(vec, gtype_s(k->s));
                 }
             }
-            printf("%ld\n",bn_size(tree));
+            gvec_traverse(cur, vec){
+                printf("Doi bong bi xuong hang la %s\n", cur->s);
+                rbm_remove(tree, gtype_s(cur->s));
+            }
+            rbm_traverse(k, v, tree){
+                printf("%s %ld\n", k->s, v->l);
+            }
         }
         else if(choice == 5){
-            FILE *f2 = fopen(argv[2], "w+");
-            bn_traverse_lnr(cur, tree){
-                name = bns_node_g_key(cur).s;
-                tmp = hmap_value(map, gtype_s(name));
-                fprintf(f2, "%s\t%ld\n",name, tmp->l);
+            FILE *out = fopen(argv[2], "w+");
+            rbm_traverse(k, v, tree){
+                fprintf(out, "%s\t%ld\n", k->s, v->l);
             }
-            fclose(f2);
+            fclose(out);
         }
     }
+    rbm_free(tree);
     fclose(f);
 }
